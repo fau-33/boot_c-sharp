0d711589c3227805091638829f9aa7e09c7ba41e
Flávio Félix <fau_felix30@hotmail.com>
2020-12-21 15:41:28 -0300

    Feito as aulas 07,08 e 09

---
+++ aulas/Aula09/aula09.cs
@@ -0,0 +1,11 @@
+using System;
+class aula09{
+  static void Main(){
+
+    int num=10;
+    num=num>>1;
+
+    Console.WriteLine(num);
+
+  }
+}
---
+++ aulas/aula07/aula07.cs
@@ -0,0 +1,11 @@
+using System;
+
+class aula07{
+
+  static void Main(){
+    const string canal="CBF Canal";
+    const double pi=3.1415;
+
+    Console.WriteLine("Canal: {0}\nPi: {1}", canal,pi);
+  }
+}
